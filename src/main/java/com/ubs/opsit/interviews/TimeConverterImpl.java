e85d1e0e9fd7eba9dadcdf5290f69a26c82ea453
vmoroz <azoga4@gmail.com>
2019-01-30 23:28:19 +0100

    Init commit. Added TimeConverterImpl

---
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -0,0 +1,11 @@
+package com.ubs.opsit.interviews;
+
+/**
+ * Created by vmoroz on 1/30/2019.
+ */
+public class TimeConverterImpl implements TimeConverter{
+    @Override
+    public String convertTime(String aTime) {
+        return "__________WRONG_VALUE_________";
+    }
+}
---
+++ src/test/java/com/ubs/opsit/interviews/BerlinClockFixture.java
@@ -0,0 +1,38 @@
+package com.ubs.opsit.interviews;
+
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.junit.Test;
+
+import static com.ubs.opsit.interviews.support.BehaviouralTestEmbedder.aBehaviouralTestRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Acceptance test class that uses the JBehave (Gerkin) syntax for writing stories.  You should not need to
+ * edit this class to complete the exercise, this is your definition of done.
+ */
+public class BerlinClockFixture {
+
+    //TODO Added new TimeConverterImpl().
+    // Not sure if it is available operation. Otherwise Reflection(?) should be used or something similar to Spring Bean Factory (?)
+    private TimeConverter berlinClock = new TimeConverterImpl(); //
+    private String theTime;
+
+    @Test
+    public void berlinClockAcceptanceTests() throws Exception {
+        aBehaviouralTestRunner()
+                .usingStepsFrom(this)
+                .withStory("berlin-clock.story")
+                .run();
+    }
+
+    @When("the time is $time")
+    public void whenTheTimeIs(String time) {
+        theTime = time;
+    }
+
+    @Then("the clock should look like $")
+    public void thenTheClockShouldLookLike(String theExpectedBerlinClockOutput) {
+        assertThat(berlinClock.convertTime(theTime)).isEqualTo(theExpectedBerlinClockOutput);
+    }
+}
